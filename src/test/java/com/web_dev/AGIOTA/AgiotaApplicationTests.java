c5e6738ba30c8a10b1ebd9e4d83c1a4b04f21040
Guilherme Felix <guilhermerutemfelix@gmail.com>
2024-07-22 18:57:06 -0300

    commit do projeto

---
+++ src/test/java/com/web_dev/AGIOTA/AgiotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_dev.AGIOTA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgiotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
